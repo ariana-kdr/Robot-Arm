71720376c66cf490fa2bc2bfbf4bb264a2ea88ed
ariana-kdr <67506710+ariana-kdr@users.noreply.github.com>
2022-05-11 14:25:54 +0200

    Add files via upload

---
+++ SPERTE_installation/Tooling/Matlab_scripts/Install_Sperte/Install_Simulink_Lib/SPERTE_Install_Simulink_Library.m
@@ -0,0 +1,18 @@
+function SPERTE_Install_Simulink_Library()
+%% SPERTE_Install_Simulink_Library()
+% Install the required simulink library on the system
+%
+% Input Arguments
+% none
+
+% Copyright (C) 2021 Enzo Evers
+% see www.enzoevers.nl for contact details
+% 
+% Control Systems Technology group
+% Departement of Mechanical Engineering
+% Eindhoven University of Technology
+% 
+% SPERTE is licensed under the GNU GPLv3
+
+end
+
